8fe86292aad17a3c1ad29dfd9354a6c6b170b2f4
JacobF94 <jacob.frensley@gmail.com>
2022-02-28 13:09:59 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+Main();
+void Main()
+{
+    GamePrompt();
+};
+
+void GamePrompt()
+{
+    Console.WriteLine("Welcome to the guessing game! Guess the secret number between 1 and 50:");
+    string input = Console.ReadLine();
+    int x;
+    bool success = int.TryParse(input, out x);
+    if (success)
+    {
+        Random r = new Random();
+        int secretNumber = r.Next(1, 50);
+        if (x == secretNumber)
+        {
+            Console.Write("Congratulations, you guessed the secret number!!!");
+        }
+        else
+        {
+            Console.Write("Sorry, you guessed wrong!!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("You didn't enter a real number!");
+    }
+};
